47ce06cf837c864aa975cbc6f10842f7bb138560
Duncan Paterson <d.paterson@me.com>
2018-04-15 23:26:46 +0200

    minor tweaks to index.html

--- src/main/resources/archetype-resources/src/main/frontend/scripts/index.js
+++ src/main/resources/archetype-resources/src/main/frontend/scripts/index.js
@@ -5,7 +5,7 @@ function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
   return element;
 }
 
